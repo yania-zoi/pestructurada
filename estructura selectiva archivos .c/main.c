050080ab57df1e0fdf37af6ccf63037febd2e5e7
yania-zoi <yaniazambu@gmail.com>
2026-02-20 15:54:11 -0600

    Add files via upload

---
+++ estructura selectiva archivos .c/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num, n;
+    printf("ingrese un numero: ");
+    scanf("%d", &num);
+    n = num%2;
+    if(n==1){
+        printf("su numero es impar.\n");
+    }
+    return 0;
+}
