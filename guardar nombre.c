dbae8abccbc482966ba3e54291859bbc0f8dcdc9
yania-zoi <yaniazambu@gmail.com>
2026-03-20 08:40:33 -0600

    Update guardar nombre.c

--- guardar nombre.c
+++ guardar nombre.c
@@ -3,15 +3,33 @@
 
 int main()
 {
-    char nombre [3][20];
-    printf("ingrese su nombre: ");
-    fgets(nombre[0],20,stdin);
-    printf("ingrese su segundo nombre: ");
-    fgets(nombre[1],20,stdin);
-    printf("ingrese su primer apellido: ");
-    fgets(nombre[2],20,stdin);
-    printf("ingrese su segundo apellido: ");
-    fgets(nombre[3],20,stdin);
+    char nombre [4][20];
+    int i, j;
+
+    for(i=0; i<4; i++){
+        printf("ingrese su nombre %d: ", i+1);
+        fgets(nombre[i],20,stdin);
+    }
+
+    i=0;
+
+    for(i=0; i<4; i++){
+        printf("%d ", &nombre[i]);
+    }
+    i= 0;
+    for(i=0; i<4; i++){
+        printf("%s ", nombre[i]);
+    }
+    i=0;
+    char arreglo [5];
+    for(i=0; i<5; i++){
+        printf("\n%d \n", &arreglo[i]);
+    }
+
+    int numeros [5];
+    for(i=0; i<5; i++){
+        printf("\n%d ", &numeros[i]);
+    }
 
     return 0;
 }
